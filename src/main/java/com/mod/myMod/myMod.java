fd15d363aeb371b8565a478e1ffcfd030024f206
fallli <daniel.padva@gmx.de>
2017-03-07 21:48:25 +0100

    initial commit

---
+++ src/main/java/com/mod/myMod/myMod.java
@@ -0,0 +1,28 @@
+package com.mod.myMod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "", name = "", version = "")
+public class myMod {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+			
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {		
+				
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+
+}
